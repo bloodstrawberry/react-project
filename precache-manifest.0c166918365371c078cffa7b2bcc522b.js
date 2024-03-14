0e195fd8586159e4b156c81e915051369e59f2b6
bloodstrawberry <vvv3334@hanmail.net>
2024-03-14 23:34:01 +0900

    Updates

---
+++ precache-manifest.0c166918365371c078cffa7b2bcc522b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f8ac93aa5b8d38fc932",
+    "url": "/react-project/static/css/main.63f469e2.chunk.css"
+  },
+  {
+    "revision": "2f8ac93aa5b8d38fc932",
+    "url": "/react-project/static/js/main.6b0c4e9b.chunk.js"
+  },
+  {
+    "revision": "76d3adc0dda5f2c03f92",
+    "url": "/react-project/static/js/runtime~main.4a5df2a0.js"
+  },
+  {
+    "revision": "2f9a93176f2fbb04564c",
+    "url": "/react-project/static/css/2.d0557b39.chunk.css"
+  },
+  {
+    "revision": "2f9a93176f2fbb04564c",
+    "url": "/react-project/static/js/2.8390f695.chunk.js"
+  },
+  {
+    "revision": "ee9af204cfe48865445e",
+    "url": "/react-project/static/js/3.ca05dd6f.chunk.js"
+  },
+  {
+    "revision": "c0afe8f189f429433080d4238ce25338",
+    "url": "/react-project/index.html"
+  }
+];
