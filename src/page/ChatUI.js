20edbd8973190e597f7dc4b25df2a6b57d51cb3c
bloodstrawberry <vvv3334@hanmail.net>
2024-03-24 17:05:55 +0900

    Chat UI onSend 구현

--- src/page/ChatUI.js
+++ src/page/ChatUI.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+
 import {
   MainContainer,
   ChatContainer,
@@ -10,79 +11,71 @@ import {
   Avatar,
 } from "@chatscope/chat-ui-kit-react";
 
+const AVATAR_IMAGE =
+  "https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory3.daumcdn.net%2Ftistory%2F4431109%2Fattach%2F3af65be1d8b64ece859b8f6d07fafadc";
+
+const defaultMessage = [
+  {
+    model: {
+      message: "How are you?",
+      direction: "incoming",
+    },
+    avatar: {
+      src: AVATAR_IMAGE,
+      name: "bloodstrawberry",
+    },
+  },
+  {
+    model: {
+      message: "I'm fine, thank you, and you?",
+      direction: "outgoing",
+    },
+  },
+  {
+    model: {
+      message: "I'm fine, too. thank you, and you?",
+      direction: "incoming",
+    },
+    avatar: {
+      src: AVATAR_IMAGE,
+      name: "bloodstrawberry",
+    },
+  },
+];
+
+const getMessageComponent = (data) => {
+  return data.map((item, index) => {
+    return (
+      <Message key={index} model={item.model}>
+        {item.avatar ? (
+          <Avatar src={item.avatar.src} name={item.avatar.name} />
+        ) : null}
+      </Message>
+    );
+  });
+};
+
 const ChatUI = () => {
+  const [messages, setMessages] = useState(defaultMessage);
+
+  const handleSend = (input) => {
+    let newMessage = {
+      model: {
+        message: input,
+        direction: "outgoing",
+      },
+    };
+
+    setMessages([...messages, newMessage]);
+  };
+
   return (
     <div>
       <div style={{ position: "relative", height: "500px" }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
-              <Message
-                model={{
-                  direction: "incoming",
-                  type: "custom",
-                }}
-              >
-                <Avatar
-                  src={
-                    "https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory3.daumcdn.net%2Ftistory%2F4431109%2Fattach%2F3af65be1d8b64ece859b8f6d07fafadc"
-                  }
-                  name="Akane"
-                />
-                <Message.CustomContent>
-                  <strong>This is strong text</strong>
-                  <br />
-                  Message content is provided as{" "}
-                  <span
-                    style={{
-                      color: "red",
-                    }}
-                  >
-                    {" "}
-                    custom elements
-                  </span>{" "}
-                  from child <strong>Message.CustomContent</strong> element
-                </Message.CustomContent>
-              </Message>
-              <p>Provided as payload</p>
-              <Message
-                model={{
-                  message: "Hello my friend",
-                  sentTime: "15 mins ago",
-                  direction: "outgoing",
-                  position: "first",
-                }}
-              />
-              <Message
-                model={{
-                  direction: "incoming",
-                  payload: (
-                    <Message.CustomContent>
-                      <strong>This is strong text</strong>
-                      <br />
-                      Message content is provided as{" "}
-                      <span
-                        style={{
-                          color: "red",
-                        }}
-                      >
-                        {" "}
-                        custom elements
-                      </span>{" "}
-                      from payload property
-                    </Message.CustomContent>
-                  ),
-                }}
-              >
-                <Avatar
-                  src={
-                    "https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory3.daumcdn.net%2Ftistory%2F4431109%2Fattach%2F3af65be1d8b64ece859b8f6d07fafadc"
-                  }
-                  name="Joe"
-                />
-              </Message>
-            </MessageList>
-            <MessageInput placeholder="Type message here" onChange={(e) => console.log(e)} onSend={(e)=> console.log(`send`, e)}/>
+            <MessageList>{getMessageComponent(messages)}</MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />
           </ChatContainer>
         </MainContainer>
       </div>
