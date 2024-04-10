3ac6f28164aab95869c3f8ab5fa3343aa303e670
bloodstrawberry <vvv3334@hanmail.net>
2024-04-10 17:44:31 +0900

    최적화 1

--- src/page/SocketTable.js
+++ src/page/SocketTable.js
@@ -39,24 +39,68 @@ const customOptions = {
     if (source === "loadData") return;
 
     console.log("Changed Data :", source, changes);
-    socketIO.emit("sendData", this.getData());
+    socketIO.emit("changeData", changes);
   },
   afterCreateRow: function (index, amount) {
     console.log("create row :", index, amount);
-    socketIO.emit("sendData", this.getData());
+    socketIO.emit("createRow", index, amount);
   },
   afterRemoveRow: function (index, amount) {
     console.log("remove row :", index, amount);
-    socketIO.emit("sendData", this.getData());
+    socketIO.emit("removeRow", index, amount);
   },
   afterCreateCol: function (index, amount) {
     console.log("create col :", index, amount);
-    socketIO.emit("sendData", this.getData());
+    socketIO.emit("createCol", index, amount);
   },
   afterRemoveCol: function (index, amount) {
     console.log("remove col :", index, amount);
-    socketIO.emit("sendData", this.getData());
+    socketIO.emit("removeCol", index, amount);
   },
+};
+
+const insertRows = (table, rowIndex, amount) => {
+  const afterIndex = table.slice(rowIndex);
+  const emptyArray = Array.from({ length: table[0].length }, () => "");
+  const newRows = Array.from({ length: amount }, () => emptyArray);  
+  const newArray = table.slice(0, rowIndex).concat(newRows, afterIndex);
+
+  return newArray;
+};
+
+const deleteRows = (table, rowIndex, amount) => {
+  const newTable = [...table]; 
+
+  newTable.splice(rowIndex, amount);
+
+  return newTable;
+};
+
+const insertColumns = (table, colIndex, amount) => {
+  const newMatrix = [];
+
+  for (let i = 0; i < table.length; i++) {
+    const newRow = [...table[i]]; 
+
+    for (let k = 0; k < amount; k++) {
+      newRow.splice(colIndex + k, 0, "");
+    }
+
+    newMatrix.push(newRow); 
+  }
+
+  return newMatrix;
+};
+
+const deleteColumns = (table, colIndex, amount) => {
+  const newTable = [];
+  for (let i = 0; i < table.length; i++) {
+    const newRow = [...table[i]]; 
+    newRow.splice(colIndex, amount); 
+    newTable.push(newRow); 
+  }
+
+  return newTable;
 };
 
 const SocketTable = () => {
@@ -76,20 +120,78 @@ const SocketTable = () => {
     setTableData(newData);
   };
 
+  const resCreateRow = (index, amount) => {
+    console.log("createRow", index, amount);
+    setTableData((prev) => {
+      let newTable = insertRows(prev, index, amount);
+      return newTable;
+    });
+  };
+  
+  const resRemoveRow = (index, amount) => {
+    console.log("removeRow", index, amount);
+    setTableData((prev) => {
+      let newTable = deleteRows(prev, index, amount);
+      return newTable;
+    });
+  };
+
+  const resCreateCol = (index, amount) => {
+    console.log("createCol", index, amount); 
+    setTableData((prev) => {
+      let newTable = insertColumns(prev, index, amount);
+      return newTable;
+    });
+  };
+
+  const resRemoveCol = (index, amount) => {
+    console.log("removeCol", index, amount);
+    setTableData((prev) => {
+      let newTable = deleteColumns(prev, index, amount);      
+      return newTable;
+    });
+  };
+
+  const resChangeData = (changes) => {
+    console.log("changeData",changes);
+    setTableData((prev) => {
+      let newTable = [...prev];
+      for(let change of changes) {
+        let [row, col, before, after] = change;
+        newTable[row][col] = after;
+      }
+      return newTable;
+    });
+  };
+
   useEffect(() => {
     socketIO.connect();
     if (!socketIO) return;
 
     socketIO.on("respondData", respondDataCallback);
-
+    socketIO.on("resCreateRow", resCreateRow);
+    socketIO.on("resRemoveRow", resRemoveRow);
+    socketIO.on("resCreateCol", resCreateCol);
+    socketIO.on("resRemoveCol", resRemoveCol);
+    socketIO.on("resChangeData", resChangeData);
+        
     return () => {
       socketIO.off("respondData", respondDataCallback);
+      socketIO.off("resCreateRow", resCreateRow);
+      socketIO.off("resRemoveRow", resRemoveRow);
+      socketIO.off("resCreateCol", resCreateCol);
+      socketIO.off("resRemoveCol", resRemoveCol);  
+      socketIO.off("resChangeData", resChangeData);            
     };
   }, []);
 
   useEffect(() => {
     socketIO.once("initData", (data) => {
+      console.log(data);
       if(data.currentData) setTableData(data.currentData);
+      else {
+        socketIO.emit("setCurrentData", tableData);
+      }
     });
   }, []);
 
